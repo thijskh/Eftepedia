99c558f958d35b387b4db0c30118933b3fde4c6c
Thijs Kinkhorst <thijs@kinkhorst.com>
2025-05-27 21:42:43 +0200

    Footer text fix

--- skins/Eftepedia004/Eftepedia004.skin.php
+++ skins/Eftepedia004/Eftepedia004.skin.php
@@ -343,7 +343,7 @@ class Eftepedia004Template
   <a href="<?=$baseUrl?>/Colofon" title="Colofon" rel="author" class="p-author">Colofon</a> &bull;
   <a href="mailto:feedback@HAALDITWEG.eftepedia.nl" title="E-mail ons" class="email u-email">E-mail</a> &bull;
   <script>var el = document.querySelector('a.email.u-email'); var m = el.href.replace('HAALDITWEG.', ''); el.href = m;</script>
-  Volg ons <a href="https://bsky.app/profile/eftepedia.nl" target="_blank" title="Eftepedia op X" class="u-twitter">Bluesky</a> of 
+  Volg ons op <a href="https://bsky.app/profile/eftepedia.nl" target="_blank" title="Eftepedia op Bluesky" class="u-twitter">Bluesky</a> of
   <a href="https://opencoaster.net/@Eftepedia" target="_blank" title="Eftepedia op Mastodon" rel="me" class="u-twitter">Mastodon</a> of 
   <a href="https://x.com/eftepedia" target="_blank" title="Eftepedia op X" class="u-twitter">&Xopf;</a>
 &bull;
